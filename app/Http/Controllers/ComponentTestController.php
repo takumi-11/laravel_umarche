d6be04acc7f24b52cc79708ef882b2fd6c02b9c6
takumi-11 <taku.11.winter.9@gmail.com>
2022-02-16 16:51:53 +0900

    component準備

---
+++ app/Http/Controllers/ComponentTestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ComponentTestController extends Controller
+{
+    public function showComponent1() {
+        return view('test.component-test1');
+    }
+
+    public function showComponent2() {
+        return view('test.component-test2');
+    }
+}
