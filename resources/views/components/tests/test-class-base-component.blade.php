52953527f624962152b2b1ed0f3f2a5a7b1692ac
takumi-11 <taku.11.winter.9@gmail.com>
2022-02-16 19:55:05 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,5 @@
+<div>
+    クラスベースのコンポーネントです
+    {{ $classMessage }}
+    <!-- Well begun is half done. - Aristotle -->
+</div>
--- resources/views/test/component-test2.blade.php
+++ resources/views/test/component-test2.blade.php
@@ -4,4 +4,5 @@
     </x-slot>
     
     コンポーネント2
+    <x-tests.test-class-base-component classMessage='クラスメッセージ' />
 </x-tests.app>
