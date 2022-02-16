ac6da3f5468e73caffab6a227b4b81a243dd3b13
takumi-11 <taku.11.winter.9@gmail.com>
2022-02-16 19:34:20 +0900

    属性バック

--- resources/views/components/tests/cart.blade.php
+++ resources/views/components/tests/cart.blade.php
@@ -4,7 +4,9 @@
     'message' => ''
 ])
 
-<div class="boder-2 shadow-md w-1/4 p-2">
+<div {{ $attributes->merge([
+    'class' => 'boder-2 shadow-md w-1/4 p-2'
+    ]) }} >
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
--- resources/views/test/component-test1.blade.php
+++ resources/views/test/component-test1.blade.php
@@ -7,5 +7,6 @@
 
     <x-tests.cart title="タイトル" content='本文' :message='$message' />
     <x-tests.cart title="タイトル2" />
+    <x-tests.cart title="css変更したい" class='font-bold' />
 
 </x-tests.app>
