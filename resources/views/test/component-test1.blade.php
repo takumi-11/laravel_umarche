c4a49efd14a71275643586c09f1930e99cd6eebe
takumi-11 <taku.11.winter.9@gmail.com>
2022-02-16 17:23:03 +0900

    名前付きスロット

--- resources/views/test/component-test1.blade.php
+++ resources/views/test/component-test1.blade.php
@@ -1,3 +1,7 @@
 <x-tests.app>
+    <x-slot name='header'>
+        header1
+    </x-slot>
+    
     コンポーネント1
 </x-tests.app>
--- resources/views/test/component-test2.blade.php
+++ resources/views/test/component-test2.blade.php
@@ -1,1 +1,7 @@
-こんぽーねんと2
+<x-tests.app>
+    <x-slot name='header'>
+        header2
+    </x-slot>
+    
+    コンポーネント2
+</x-tests.app>
